e357c0e6ac9f845cee81cf48235449d4a2823707
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-09 23:17:31 -0300

    finish send email endpoint

--- SiteWebAssembly/SiteWebAssembly.Api/Controllers/SendEmailController.cs
+++ SiteWebAssembly/SiteWebAssembly.Api/Controllers/SendEmailController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SiteWebAssembly.Api.Services;
 using SiteWebAssembly.Model;
 
 namespace SiteWebAssembly.Api.Controllers
@@ -14,18 +15,40 @@ namespace SiteWebAssembly.Api.Controllers
     public class SendEmailController : ControllerBase
     {
         private readonly ILogger<SendEmailController> _logger;
+        private readonly ISendEmailService _sendEmailService;
 
-        public SendEmailController(ILogger<SendEmailController> logger)
+        public SendEmailController(ILogger<SendEmailController> logger, ISendEmailService sendEmailService)
         {
-            _logger = logger;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _sendEmailService = sendEmailService ?? throw new ArgumentNullException(nameof(sendEmailService));
         }
 
         [HttpPost("contact")]
-        public async Task SendEmail ([FromBody] Contact contact)
+        public async Task<ActionResult> SendEmail ([FromBody] Contact contact)
         {
-            if (string.IsNullOrEmpty(contact.Email) || string.IsNullOrEmpty(contact.Message) || string.IsNullOrEmpty(contact.Name))
+            try
             {
+                if (string.IsNullOrEmpty(contact.Email) || string.IsNullOrEmpty(contact.Message) || string.IsNullOrEmpty(contact.Name))
+                {
+                    _logger.LogDebug("Validation Error");
+                    return BadRequest();
+                }
 
+                bool response = await _sendEmailService.SendEmail(contact);
+                if (response)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    _logger.LogError($"Error in {nameof(_sendEmailService)} service");
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
     }
