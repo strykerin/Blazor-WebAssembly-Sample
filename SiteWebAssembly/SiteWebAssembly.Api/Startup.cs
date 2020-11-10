8ecdb846e4a27bf820a2299d901cd3ed92fc7f4c
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-09 23:07:33 -0300

    add in DI container SendGrid and SendEmail service

--- SiteWebAssembly/SiteWebAssembly.Api/Startup.cs
+++ SiteWebAssembly/SiteWebAssembly.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SendGrid.Extensions.DependencyInjection;
+using SiteWebAssembly.Api.Services;
 
 namespace SiteWebAssembly.Api
 {
@@ -26,6 +21,8 @@ namespace SiteWebAssembly.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSendGrid(opt => opt.ApiKey = Configuration["SendGrid:ApiKey"]);
+            services.AddScoped<ISendEmailService, SendEmailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
