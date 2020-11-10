2354d8948853f00b92bae98f38a0a53f33f27b92
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-09 22:59:12 -0300

    create endpoint to send email

---
+++ SiteWebAssembly/SiteWebAssembly.Api/Controllers/SendEmailController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SiteWebAssembly.Model;
+
+namespace SiteWebAssembly.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendEmailController : ControllerBase
+    {
+        private readonly ILogger<SendEmailController> _logger;
+
+        public SendEmailController(ILogger<SendEmailController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("contact")]
+        public async Task SendEmail ([FromBody] Contact contact)
+        {
+            if (string.IsNullOrEmpty(contact.Email) || string.IsNullOrEmpty(contact.Message) || string.IsNullOrEmpty(contact.Name))
+            {
+
+            }
+        }
+    }
+}
