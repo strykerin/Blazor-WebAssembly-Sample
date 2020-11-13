fadd8445db6bb665fd43c1620bc7f04c0a3d5920
Fabio Costa <dacosta.pereirafabio@gmail.com>
2020-11-12 22:07:34 -0300

    Create EmailService on webassembly project

---
+++ SiteWebAssembly/SiteWebAssembly/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using SiteWebAssembly.Model;
+using System.Threading.Tasks;
+
+namespace SiteWebAssembly
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Contact contactForm);
+    }
+}
